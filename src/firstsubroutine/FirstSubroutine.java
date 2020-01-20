d5766652f7c3383cf9cae3bf0638db1b407a7c1a
Densitie <55253212+Densitie@users.noreply.github.com>
2020-01-19 20:03:17 -0500

    Update FirstSubroutine.java

--- src/firstsubroutine/FirstSubroutine.java
+++ src/firstsubroutine/FirstSubroutine.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Arun k
+ * This program is subroutine making practice
+ * Oct 30 2019
  */
 package firstsubroutine;
 import java.util.Scanner;
